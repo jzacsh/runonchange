jzacsh/runonchange

commit ab09b7b86c7e14b73f29bccb03c0c83341bd58f1

bugfix: need similar "if clobber" logic for file events

run.go
@@ -208,6 +208,10 @@ func (run *runDirective) handleFSEvents(in chan fsnotify.Event) {
 				color.New(color.Bold, color.FgBlue).Sprintf("warning"))
 
 		case ev := <-in:
+			if !run.Features[flgClobberCommands] {
+				continue
+			}
+
 			if ran, _ := run.maybeRun(&ev, true /*msgStdout*/); !ran {
 				fmt.Fprintf(os.Stderr, ".")
 			}

commit 4de26f10c27e7074af437d5a99ab06b8e72ccf73

bugfix: ab09b7b86c7 caused NO file event handlign to oocur

run.go
@@ -208,7 +208,7 @@ func (run *runDirective) handleFSEvents(in chan fsnotify.Event) {
 				color.New(color.Bold, color.FgBlue).Sprintf("warning"))
 
 		case ev := <-in:
-			if !run.Features[flgClobberCommands] {
+			if run.Living != nil && !run.Features[flgClobberCommands] {
 				continue
 			}
 